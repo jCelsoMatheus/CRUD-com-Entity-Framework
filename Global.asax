ce47ddd6e6e46fc222a8413693147ba445a147e9
jCelsoMatheus <Celso_rorato@hotmail.com>
2020-01-30 15:37:25 -0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUD_MVCEF.MvcApplication" Language="C#" %>
---
+++ Models/pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MVCEF.Models
+{
+    public class pessoa
+    {
+        //Atribuir a Referência para o Entity entender que o Código é uma chave primária
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
